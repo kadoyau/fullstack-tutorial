31b3318a9087c1c79abeee697e50f629754bb0d7
kadoyau <el.psi.congroo@gmail.com>
2019-03-04 01:58:50 +0900

    チュートリアルにない、動かすのに足りない処理を追加

--- start/client/src/pages/launches.js
+++ start/client/src/pages/launches.js
@@ -6,6 +6,7 @@ import { LaunchTile, Header, Button, Loading } from '../components';
 
 export const LAUNCH_TILE_DATA = gql`
   fragment LaunchTile on Launch {
+    __typename
     id
     isBooked
     rocket {
@@ -19,8 +20,8 @@ export const LAUNCH_TILE_DATA = gql`
   }
 `;
 
-const GET_LAUNCHES = gql`
-  query launchList($after: String) {
+export const GET_LAUNCHES = gql`
+  query GetLaunchList($after: String) {
     launches(after: $after) {
       cursor
       hasMore
@@ -29,58 +30,54 @@ const GET_LAUNCHES = gql`
       }
     }
   }
+  ${LAUNCH_TILE_DATA}
 `;
 
-
 export default function Launches() {
-    return (
-      <Query query={GET_LAUNCHES}>
-        {({ data, loading, error, fetchMore }) => {
-          if (loading) return <Loading />;
-          if (error) return <p>ERROR</p>;
-  
-          return (
-            <Fragment>
-              <Header />
-              {data.launches &&
-                data.launches.launches &&
-                data.launches.launches.map(launch => (
-                  <LaunchTile
-                    key={launch.id}
-                    launch={launch}
-                  />
-                ))}
-                {data.launches &&
-                    data.launches.hasMore && (
-                      <Button
-                        onClick={() =>
-                          fetchMore({
-                            variables: {
-                              after: data.launches.cursor,
-                            },
-                            updateQuery: (prev, { fetchMoreResult, ...rest }) => {
-                              if (!fetchMoreResult) return prev;
-                              return {
-                                ...fetchMoreResult,
-                                launches: {
-                                  ...fetchMoreResult.launches,
-                                  launches: [
-                                    ...prev.launches.launches,
-                                    ...fetchMoreResult.launches.launches,
-                                  ],
-                                },
-                              };
-                            },
-                          })
-                        }
-                      >
-                        Load More
-                      </Button>
-                    )
-                  }                
-            </Fragment>
-          );
-        }}
-      </Query>
-    );
-  };
+  return (
+    <Query query={GET_LAUNCHES}>
+      {({ data, loading, error, fetchMore }) => {
+        if (loading) return <Loading />;
+        if (error) return <p>ERROR</p>;
+
+        return (
+          <Fragment>
+            <Header />
+            {data.launches &&
+              data.launches.launches &&
+              data.launches.launches.map(launch => (
+                <LaunchTile key={launch.id} launch={launch} />
+              ))}
+            {data.launches &&
+              data.launches.hasMore && (
+                <Button
+                  onClick={() =>
+                    fetchMore({
+                      variables: {
+                        after: data.launches.cursor,
+                      },
+                      updateQuery: (prev, { fetchMoreResult, ...rest }) => {
+                        if (!fetchMoreResult) return prev;
+                        return {
+                          ...fetchMoreResult,
+                          launches: {
+                            ...fetchMoreResult.launches,
+                            launches: [
+                              ...prev.launches.launches,
+                              ...fetchMoreResult.launches.launches,
+                            ],
+                          },
+                        };
+                      },
+                    })
+                  }
+                >
+                  Load More
+                </Button>
+              )}
+          </Fragment>
+        );
+      }}
+    </Query>
+  );
+}
